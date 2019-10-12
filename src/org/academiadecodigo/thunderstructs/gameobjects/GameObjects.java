e5e15d779309c93e996d4203c0ef4398fa78f083
Beatriz <beatrizalmeidapires@hotmail.com>
2019-10-12 01:05:10 +0000

    Reorganized methods from subclass Person to superclass GameObjects

--- src/org/academiadecodigo/thunderstructs/gameobjects/GameObjects.java
+++ src/org/academiadecodigo/thunderstructs/gameobjects/GameObjects.java
@@ -1,7 +1,25 @@
 package org.academiadecodigo.thunderstructs.gameobjects;
 
-public class GameObjects implements Physics {
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.thunderstructs.Game;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
+import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
 
+public class GameObjects implements Physics, KeyboardHandler {
+
+    private Position position;
+    private Rectangle person;
+
+    public GameObjects(Position position, Rectangle person){
+        this.position = position;
+        this.person = person;
+    }
+
+    public void drawPerson() {
+        person.setColor(Color.YELLOW);
+        person.fill();
+    }
 
     @Override
     public void collision() {
@@ -10,6 +28,59 @@ public class GameObjects implements Physics {
 
     @Override
     public void gravity() {
+
+    }
+
+    public void moveUp() {
+
+        if(!(position.getPosY() <=  Game.margin)) {
+            person.translate(0, -10);
+            position.setPosY(position.getPosY() - 10);
+        }
+    }
+
+    public void moveDown() {
+
+        if(!(position.getPosY() >= 800 - 40)) {
+            person.translate(0, 10);
+            position.setPosY(position.getPosY() + 10);
+        }
+    }
+
+    public void moveLeft() {
+        if(!(position.getPosX() <= Game.margin)) {
+            person.translate(-10, 0);
+            position.setPosX(position.getPosX() - 10);
+        }
+    }
+
+    public void moveRight() {
+        if(!(position.getPosX() >= 1500 - 60 )) {
+            person.translate(10, 0);
+            position.setPosX(position.getPosX() + 10);
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyboardEvent keyboardEvent) {
+        switch (keyboardEvent.getKey()) {
+            case KeyboardEvent.KEY_LEFT:
+                moveLeft();
+                break;
+            case KeyboardEvent.KEY_RIGHT:
+                moveRight();
+                break;
+            case KeyboardEvent.KEY_DOWN:
+                moveDown();
+                break;
+            case KeyboardEvent.KEY_UP:
+                moveUp();
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyboardEvent keyboardEvent) {
 
     }
 }
--- src/org/academiadecodigo/thunderstructs/gameobjects/Person.java
+++ src/org/academiadecodigo/thunderstructs/gameobjects/Person.java
@@ -1,78 +1,15 @@
 package org.academiadecodigo.thunderstructs.gameobjects;
 
-import org.academiadecodigo.simplegraphics.graphics.Color;
 import org.academiadecodigo.simplegraphics.graphics.Rectangle;
-import org.academiadecodigo.simplegraphics.keyboard.KeyboardEvent;
-import org.academiadecodigo.simplegraphics.keyboard.KeyboardHandler;
-import org.academiadecodigo.thunderstructs.Game;
 
-public class Person implements KeyboardHandler {
+public class Person extends GameObjects {
 
-    private Rectangle person;
     private Position position;
 
     public Person(Position position) {
+
+        super(position, new Rectangle(position.getPosX(), position.getPosY(), 70, 50));
         this.position = position;
-    }
-
-    public void drawPerson() {
-
-        person = new Rectangle(position.getPosX(), position.getPosY(), 70, 50);
-        person.setColor(Color.YELLOW);
-        person.fill();
-    }
-
-    @Override
-    public void keyPressed(KeyboardEvent keyboardEvent) {
-        switch (keyboardEvent.getKey()) {
-            case KeyboardEvent.KEY_LEFT:
-                moveLeft();
-                break;
-            case KeyboardEvent.KEY_RIGHT:
-                moveRight();
-                break;
-            case KeyboardEvent.KEY_DOWN:
-                moveDown();
-                break;
-            case KeyboardEvent.KEY_UP:
-                moveUp();
-                break;
-        }
-    }
-
-    public void moveUp() {
-
-        if(!(position.getPosY() <=  Game.margin)) {
-            person.translate(0, -10);
-            position.setPosY(position.getPosY() - 10);
-        }
-    }
-
-    public void moveDown() {
-
-        if(!(position.getPosY() >= 800 - 40)) {
-            person.translate(0, 10);
-            position.setPosY(position.getPosY() + 10);
-        }
-    }
-
-    public void moveLeft() {
-        if(!(position.getPosX() <= Game.margin)) {
-            person.translate(-10, 0);
-            position.setPosX(position.getPosX() - 10);
-        }
-    }
-
-    public void moveRight() {
-        if(!(position.getPosX() >= 1500 - 60 )) {
-            person.translate(10, 0);
-            position.setPosX(position.getPosX() + 10);
-        }
-    }
-
-
-    @Override
-    public void keyReleased(KeyboardEvent keyboardEvent) {
 
     }
 
@@ -83,6 +20,5 @@ public class Person implements KeyboardHandler {
     public int getPosY() {
         return this.position.getPosY();
     }
-
 
 }
